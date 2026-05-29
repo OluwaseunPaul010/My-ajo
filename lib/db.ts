1547d0e30a0e387156c9ed8196fb6644964f2d6a
Oluwaseun Paul <ikuyinminuoluwaseun93@gmail.com>
2026-05-29 13:32:52 +0200

    Fix Prisma config for Vercel

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-
-const connectionString = process.env.DATABASE_URL!;
-const adapter = new PrismaNeon({ connectionString });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -10,6 +6,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({ adapter } as any);
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
